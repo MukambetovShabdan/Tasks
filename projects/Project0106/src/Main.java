27d227fe1662b03ebcc5629a95972bc5121a3fb3
MukambetovShabdan <mukambetov_s@auca.kg>
2018-12-03 16:19:02 +0600

    Project0106

--- projects/Project0106/src/Main.java
+++ projects/Project0106/src/Main.java
@@ -1,6 +1,7 @@
 import processing.core.PApplet;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
 
 public class Main extends PApplet {
@@ -11,35 +12,45 @@ public class Main extends PApplet {
     float x;
     float y;
     float alpha = 0;
-    float beta = 360;
+    float beta;
 
     public void settings() {
         fullScreen();
     }
 
     public void setup() {
-        x = width / 10;
-        y = height / 10;
-        frameRate(100);
+        x = width / 3;
+        y = height / 12;
+        frameRate(30);
+        noStroke();
+
     }
 
 
     public void draw() {
-        background(0);
+//        fill (0, 20, 70, 0.1f);
+//        rect(0,0,width,height);
+        background(0, 20, 70, 10);
         pushMatrix();
         translate(width / 2, height / 2);
         rotate(alpha);
         alpha += 0.03;
         for (int i = 1; i <= num; i++) {
-            fill(255, 255, 0);
+            fill(250, 250,0);
+
+
             ellipse(0, 0, x, y);
             rotate(beta);
-            beta = beta - beta / num;
-
+            beta = PI/num;
         }
 
 
+
+
+
+
         popMatrix();
+
     }
 
     public static void main(String... args) {
@@ -47,3 +58,23 @@ public class Main extends PApplet {
         PApplet.main("Main");
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
